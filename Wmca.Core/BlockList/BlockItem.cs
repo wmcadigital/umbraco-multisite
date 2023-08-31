c53642593506f40e2f3d4867e965175ec11e3748
Nathan Rogan <rolorogan@gmail.com>
2023-08-31 12:47:16 +0100

    Initial setup

---
+++ Wmca.Core/BlockList/BlockItem.cs
@@ -0,0 +1,24 @@
+﻿using Umbraco.Cms.Core.Models.Blocks;
+using Umbraco.Cms.Core.Models.PublishedContent;
+
+namespace Wmca.Core.BlockList
+{
+    public class BlockItem
+    {
+
+        public Guid ContentKey { get; }
+
+        public string Type { get; }
+
+        public IPublishedElement Content { get; }
+
+        public BlockItem(BlockListItem blockListItem)
+        {
+            ContentKey = blockListItem.Content.Key;
+            Type = blockListItem.Content.ContentType.Alias;
+            Content = blockListItem.Content;
+        }
+
+    }
+}
+
---
+++ Wmca.Core/BlockList/BlockList.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using Limbo.Umbraco.BlockList.Converters;
+using Limbo.Umbraco.BlockList.PropertyEditors;
+using Umbraco.Cms.Core.Models.Blocks;
+using Umbraco.Cms.Core.Models.PublishedContent;
+
+namespace Wmca.Core.BlockList
+{
+    public class BlockListTypeConverter : IBlockListTypeConverter
+    {
+
+        public string Name => "Block Item Converter";
+
+        public Type GetType(IPublishedPropertyType propertyType, LimboBlockListConfiguration config)
+        {
+            return config.IsSinglePicker ? typeof(BlockItem) : typeof(IReadOnlyList<BlockItem>);
+        }
+
+        public object? Convert(IPublishedElement owner, IPublishedPropertyType propertyType, BlockListModel? source, LimboBlockListConfiguration config)
+        {
+
+            if (source == null) return config.IsSinglePicker ? null : Array.Empty<BlockItem>();
+
+            if (!config.IsSinglePicker) return source.Select(x => new BlockItem(x)).ToArray();
+
+            BlockListItem? first = source.FirstOrDefault();
+            return first == null ? null : new BlockItem(first);
+
+        }
+
+    }
+}
+
