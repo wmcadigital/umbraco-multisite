27ba429f3592746a5f9857cc959785b4301a7f35
GeorgeEverett <george.everett@wmca.org.uk>
2023-12-08 16:50:34 +0000

    Content updates

--- Wmca.Models/Content/HomepageMidlandsConnect.generated.cs
+++ Wmca.Models/Content/HomepageMidlandsConnect.generated.cs
@@ -50,6 +50,14 @@ namespace Wmca.Models.Content
 		// properties
 
 		///<summary>
+		/// Homepage Content
+		///</summary>
+		[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Umbraco.ModelsBuilder.Embedded", "12.0.1+20a4e47")]
+		[global::System.Diagnostics.CodeAnalysis.MaybeNull]
+		[ImplementPropertyType("homepageContent")]
+		public virtual global::Umbraco.Cms.Core.Models.Blocks.BlockGridModel HomepageContent => this.Value<global::Umbraco.Cms.Core.Models.Blocks.BlockGridModel>(_publishedValueFallback, "homepageContent");
+
+		///<summary>
 		/// Copy
 		///</summary>
 		[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Umbraco.ModelsBuilder.Embedded", "12.0.1+20a4e47")]
