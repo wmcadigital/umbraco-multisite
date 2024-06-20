ba122e038a254e631d8942cb8732c9d966d34fcf
Nathan Rogan <rolorogan@gmail.com>
2024-06-20 11:07:13 +0100

    add backoffice assets

---
+++ Wmca.Web/App_Plugins/Backoffice/js/backoffice.js
@@ -0,0 +1,11 @@
+
+    console.log('load sprites bo');
+    const ajax = new XMLHttpRequest();
+    ajax.open('GET', 'https://cloudcdn.wmca.org.uk/designsystem/0.0.3/img/ds-icons.min.svg', true);
+    ajax.send();
+    ajax.onload = function () {
+      const div = document.createElement('div');
+      div.style.display = 'none';
+      div.innerHTML = ajax.responseText;
+      document.body.insertBefore(div, document.body.childNodes[0]);
+    };
