3070cf7ee81c76eed1377910b5b8334d60e06c70
Nathan Rogan <rolorogan@gmail.com>
2024-07-22 09:14:01 +0100

    fix redirects

--- Wmca.Web/App_Plugins/UFormKit/scripts/uFormKit.js
+++
@@ -1,9 +0,0 @@
-﻿function makeExclusive(element) {
-    var isChecked = element.checked;
-
-    if (isChecked) {
-        document.getElementsByName(element.getAttribute("name")).forEach(el => el.checked = false);;
-        element.checked = true;
-    }
-}
-
--- Wmca.Web/Program.cs
+++ Wmca.Web/Program.cs
@@ -1,20 +1,54 @@
-﻿namespace Wmca
-{
-    public class Program
+﻿WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+builder.CreateUmbracoBuilder()
+    .AddBackOffice()
+    .AddWebsite()
+    .AddDeliveryApi()
+    .AddComposers()
+    .AddAzureBlobMediaFileSystem()
+    .AddAzureBlobImageSharpCache()
+    .Build();
+
+WebApplication app = builder.Build();
+
+await app.BootUmbracoAsync();
+
+#if (UseHttpsRedirect)
+app.UseHttpsRedirection();
+#endif
+
+app.UseUmbraco()
+    // .WithMiddleware(u =>
+    // {
+    //     u.UseBackOffice();
+    //     u.UseWebsite();
+    // })
+    .WithCustomMiddleware(u =>
     {
-        public static void Main(string[] args)
-            => CreateHostBuilder(args)
-                .Build()
-                .Run();
+        u.RunPrePipeline();
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureUmbracoDefaults()
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStaticWebAssets();
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
-}
+        u.UseUmbracoCoreMiddleware();
+        u.AppBuilder.UseUmbracoMediaFileProvider();
+        u.AppBuilder.UseStaticFiles();
+        u.AppBuilder.UseUmbracoPluginsStaticFiles();
+        u.AppBuilder.UseRouting();
+        // u.AppBuilder.UseCors(MyAllowSpecificOrigins);
+        u.AppBuilder.UseAuthentication();
+        u.AppBuilder.UseAuthorization();
+        u.AppBuilder.UseRequestLocalization();
+        u.AppBuilder.UseSession();
 
+        u.RunPreRouting();
+        u.RunPostRouting();
+        u.RunPostPipeline();
+        u.UseBackOffice();
+        u.UseWebsite();
+    })
+    .WithEndpoints(u =>
+    {
+        u.UseInstallerEndpoints();
+        u.UseBackOfficeEndpoints();
+        u.UseWebsiteEndpoints();
+    });
+
+await app.RunAsync();
